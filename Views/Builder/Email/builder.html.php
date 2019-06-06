0fbe67eacbe08895f98d6b5a166346c134b31729
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-06-06 22:05:24 +0200

    Revert "Added unit test Params and ParamsTransformer"
    This reverts commit 83c21bf1d5b07908a4f201d6f135c17cc2c16c1d.

--- Views/Builder/Email/builder.html.php
+++ Views/Builder/Email/builder.html.php
@@ -13,6 +13,7 @@
     if (m) {
         bodytext = m[1];
     }
+    console.log(bodytext);
     var editor = grapesjs.init({
         height: '100%',
         noticeOnUnload: 0,
@@ -33,25 +34,8 @@
             modalTitle: 'Select Image',
         },
 
-        plugins: ['gjs-plugin-ckeditor', 'grapesjs-parser-postcss', 'gjs-preset-newsletter'],
+        plugins: ['grapesjs-parser-postcss', 'gjs-preset-newsletter'],
         pluginsOpts: {
-            'gjs-plugin-ckeditor': {
-                position: 'center',
-                options: {
-                    startupFocus: true,
-                    extraAllowedContent: '*(*);*{*}', // Allows any class and any inline style
-                    allowedContent: true, // Disable auto-formatting, class removing, etc.
-                    enterMode: CKEDITOR.ENTER_BR,
-                    extraPlugins: 'sharedspace,justify,colorbutton,panelbutton,font',
-                    toolbar: [
-                        { name: 'styles', items: ['Font', 'FontSize' ] },
-                        ['Bold', 'Italic', 'Underline', 'Strike'],
-                        {name: 'paragraph', items : [ 'NumberedList', 'BulletedList']},
-                        {name: 'links', items: ['Link', 'Unlink']},
-                        {name: 'colors', items: [ 'TextColor', 'BGColor' ]},
-                    ],
-                }
-            },
             'gjs-preset-newsletter': {
                 modalLabelImport: 'Paste all your code here below and click import',
                 modalLabelExport: 'Copy the code and use it wherever you want',
@@ -69,151 +53,6 @@
             }
         },
     });
-
-    editor.on('load',
-        function() {
-            var PLACEHOLDERS = [{
-                id: 1,
-                name: 'address',
-                title: '{Address}',
-                description: 'Customer Support correspondence address.'
-            },
-                {
-                    id: 2,
-                    name: 'assignee',
-                    title: 'Assignee Name',
-                    description: 'Ticket assignee name.'
-                },
-                {
-                    id: 3,
-                    name: 'deadline',
-                    title: 'Deadline Time',
-                    description: 'Utmost time to which technician should handle the issue.'
-                },
-                {
-                    id: 4,
-                    name: 'department',
-                    title: 'Department Name',
-                    description: 'Department name responsible for servicing this ticket.'
-                },
-                {
-                    id: 5,
-                    name: 'caseid',
-                    title: 'Case ID',
-                    description: 'Unique case number used to distinguish tickets.'
-                },
-                {
-                    id: 6,
-                    name: 'casename',
-                    title: 'Case Name',
-                    description: 'Name of the ticket provided by the user.'
-                },
-                {
-                    id: 7,
-                    name: 'contact',
-                    title: 'Contact E-mail',
-                    description: 'Customer Support contact e-mail address.'
-                },
-                {
-                    id: 8,
-                    name: 'customer',
-                    title: 'Customer Name',
-                    description: 'Receipent of your response.'
-                },
-                {
-                    id: 9,
-                    name: 'hotline',
-                    title: 'Hotline Number',
-                    description: 'Customer Support Hotline number.'
-                },
-                {
-                    id: 10,
-                    name: 'technician',
-                    title: 'Technician Name',
-                    description: 'Technician which will handle this ticket.'
-                }
-            ];
-
-            CKEDITOR.addCss('span > .cke_placeholder { background-color: #ffeec2; }');
-
-            CKEDITOR.replace('editor1', {
-                plugins: 'autocomplete,textmatch,toolbar,wysiwygarea,basicstyles,link,undo,placeholder',
-                toolbar: [{
-                    name: 'document',
-                    items: ['Undo', 'Redo']
-                },
-                    {
-                        name: 'basicstyles',
-                        items: ['Bold', 'Italic']
-                    },
-                    {
-                        name: 'links',
-                        items: ['Link', 'Unlink']
-                    }
-                ],
-                on: {
-                    instanceReady: function(evt) {
-                        var itemTemplate = '<li data-id="{id}">' +
-                                '<div><strong class="item-title">{title}</strong></div>' +
-                                '<div><i>{description}</i></div>' +
-                                '</li>',
-                            outputTemplate = '{title}<span>&nbsp;</span>';
-
-                        var autocomplete = new CKEDITOR.plugins.autocomplete(evt.editor, {
-                            textTestCallback: textTestCallback,
-                            dataCallback: dataCallback,
-                            itemTemplate: itemTemplate,
-                            outputTemplate: outputTemplate
-                        });
-
-                        // Override default getHtmlToInsert to enable rich content output.
-                        autocomplete.getHtmlToInsert = function(item) {
-                            return this.outputTemplate.output(item);
-                        }
-                    }
-                }
-            });
-
-            function textTestCallback(range) {
-                if (!range.collapsed) {
-                    return null;
-                }
-
-                return CKEDITOR.plugins.textMatch.match(range, matchCallback);
-            }
-
-            function matchCallback(text, offset) {
-                var pattern = /\{([A-z]|\})*$/,
-                    match = text.slice(0, offset)
-                        .match(pattern);
-                if (!match) {
-                    return null;
-                }
-
-                return {
-                    start: match.index,
-                    end: offset
-                };
-            }
-
-            function dataCallback(matchInfo, callback) {
-                var data = PLACEHOLDERS.filter(function(item) {
-                    var itemName = '{' + item.name + '}';
-                    return itemName.indexOf(matchInfo.query.toLowerCase()) == 0;
-                });
-
-                callback(data);
-            }
-            });
-   /* var blockManager = editor.BlockManager;
-    blockManager.add('some-block-id', {
-            render: ({ el }) => {
-            const btn = document.createElement('button');
-    btn.innerHTML = 'Click me';
-    btn.addEventListener('click', () => alert('Do something'))
-    el.appendChild(btn);
-    },
-    });*/
     var pnm = editor.Panels;
     pnm.removeButton("options", "gjs-open-import-template");
     pnm.removeButton("options", "gjs-toggle-images");
@@ -238,6 +77,7 @@
             label: 'Save and close',
             command: function (editor1, sender) {
                 var newContent = ($('textarea#templateBuilder').val()).replace('||BODY||', editor.runCommand('gjs-get-inlined-html'));
+                console.log(newContent);
                 window.opener.mQuery('textarea.builder-html').val(newContent);
                 window.close();
             },
--- Views/Builder/Email/head.html.php
+++ Views/Builder/Email/head.html.php
@@ -3,8 +3,6 @@
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="//unpkg.com/grapesjs@0.14.57/dist/grapes.min.js"></script>
-<script src="https://cdn.ckeditor.com/4.11.4/standard-all/ckeditor.js"></script>
-<script src="https://grapesjs.com/js/grapesjs-plugin-ckeditor.min.js"></script>
 <script src="https://unpkg.com/grapesjs-parser-postcss"></script>
 <script src="https://unpkg.com/grapesjs-preset-newsletter@0.2.20/dist/grapesjs-preset-newsletter.min.js"></script>
 
